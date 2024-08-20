e1c678beffdb21386a517981ebb7daf759f230ea
TomaszPe <tomasz.pawlowski@cyberalpaca.com>
2024-08-20 14:02:49 +0200

    Add gui test case with typing

--- tests/gui_tests/interaction/tst_guitests.cpp
+++ tests/gui_tests/interaction/tst_guitests.cpp
@@ -19,6 +19,7 @@ private:
     QPointer<QApplication> app;
     QPointer<QQmlApplicationEngine> engine;
     void steps_test_one();
+    void steps_typeLogin();
 
 public:
     ButtonInteraction();
@@ -26,6 +27,7 @@ public:
 
 private slots:
     void test_one();
+    void test_typeLogin();
 };
 
 ButtonInteraction::ButtonInteraction()
@@ -56,6 +58,38 @@ void ButtonInteraction::test_one()
     }
     QTimer::singleShot(3000, this, &ButtonInteraction::steps_test_one);
     app->exec();
+}
+
+void ButtonInteraction::test_typeLogin()
+{
+    auto [app, engine] = initializeApplication(0, nullptr);
+    this->app = app;
+    this->engine = engine;
+
+    if (!app || !engine) {
+        exit(1);
+    }
+    QTimer::singleShot(3000, this, &ButtonInteraction::steps_typeLogin);
+    app->exec();
+}
+
+void ButtonInteraction::steps_typeLogin()
+{
+    Automator automator(engine);
+
+    QQuickItem *myInputField = qobject_cast<QQuickItem*>(automator.findObject("myInputField"));
+
+    QVERIFY2(myInputField, "myInputField should be found");
+    QVERIFY2(myInputField->isVisible(), "myInputField should be visible");
+    automator.typeText(myInputField, "alpaca@cyberalpaca.com");
+
+    QTest::qSleep(2000);
+    QVariant textProperty = myInputField->property("text");
+    QString txt = textProperty.toString();
+
+    QCOMPARE(txt, "alpaca@cyberalpaca.com");
+
+    QTest::qSleep(2000);
 }
 
 #include "tst_guitests.moc"
