6aecb706da3a3a2852756e69b1bad17497e84a83
TomaszPe <tomasz.pawlowski@cyberalpaca.com>
2024-08-26 15:33:30 +0200

    Use wait from automator

--- tests/gui_tests/catch_login/test_example.cpp
+++ tests/gui_tests/catch_login/test_example.cpp
@@ -2,7 +2,6 @@
 #include <catch2/matchers/catch_matchers_all.hpp>
 #include <suri/automator.h>
 #include "../../../src/application_init.h"
-#include <QTest>
 #include <QQuickItem>
 
 Automator automator;
@@ -21,7 +20,8 @@ public:
 
     ~TestFixture() {
         // Cleanup code - optional
-        automator.closeApp();
+        // Looks like we don't need to close up, it closes automatically
+        // automator.closeApp();
     }
 
     void initTestCase() {
@@ -31,7 +31,7 @@ public:
             exit(1);
         }
 
-        QTest::qSleep(5000);
+        automator.wait(5000);
         automator.setEngine(engine);
     }
 
@@ -47,15 +47,14 @@ TEST_CASE_METHOD(TestFixture, "Example test", "[example]") {
 
         INFO("Click forgotPasswordLink");
         automator.click(forgotPasswordLink);
-
-        QTest::qSleep(1000);
+        automator.wait(1000);
 
         QQuickItem *backFromForgotPassword = qobject_cast<QQuickItem*>(automator.findObject("backFromForgotPassword"));
         REQUIRE(backFromForgotPassword); //"backFromForgotPassword should be found"
 
         INFO("Click backFromForgotPassword");
         automator.click(backFromForgotPassword);
-        QTest::qSleep(1000);
+        automator.wait(1000);
 
     }
 
@@ -66,16 +65,16 @@ TEST_CASE_METHOD(TestFixture, "Example test", "[example]") {
         REQUIRE(myInputField->isVisible());// "myInputField should be visible"
         automator.typeText(myInputField, "alpaca@cyberalpaca.com");
 
-        QTest::qSleep(1000);
+        automator.wait(1000);
         QVariant textProperty = myInputField->property("text");
         QString txt = textProperty.toString();
 
         REQUIRE_THAT(txt.toStdString(), Catch::Matchers::Equals("alpaca@cyberalpaca.com"));
 
-        QTest::qSleep(1000);
+        automator.wait(1000);
         automator.click(myInputField);
 
-        QTest::qSleep(1000);
+        automator.wait(1000);
 
     }
 
