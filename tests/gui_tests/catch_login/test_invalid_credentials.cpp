d3a004359b7443f3d6e57a99975e97f96552bc93
Adakar <jakub.topolski@cyberalpaca.com>
2024-08-28 14:53:50 +0200

    Make Catch based tests more verbose

--- tests/gui_tests/catch_login/test_invalid_credentials.cpp
+++ tests/gui_tests/catch_login/test_invalid_credentials.cpp
@@ -3,15 +3,16 @@
 #include <suri/automator.h>
 #include <QQuickItem>
 #include "TestFixture.h"
+#include "macros.h"
 
 TEST_CASE_METHOD(TestFixture, "Login with invalid credentials", "[example]") {
 
     QQuickItem *emailInput = qobject_cast<QQuickItem*>(automator.findObject("emailInput_textInput"));
     QQuickItem *passwordInput = qobject_cast<QQuickItem*>(automator.findObject("passwordInput_textInput"));
-    REQUIRE(emailInput); // emailInput should be found
-    REQUIRE(emailInput->isVisible()); // emailInput should be visible
-    REQUIRE(passwordInput); // passwordInput should be found
-    REQUIRE(passwordInput->isVisible()); //passwordInput should be visible
+    REQUIRE_MSG(emailInput, "emailInput should be found");
+    REQUIRE_MSG(emailInput->isVisible(), "emailInput should be visible");
+    REQUIRE_MSG(passwordInput, "passwordInput should be found");
+    REQUIRE_MSG(passwordInput->isVisible(), "passwordInput should be visible");
 
     INFO("Type alpaca@cyberalpaca.com to emailInput");
     automator.typeText(emailInput, "alpaca@cyberalpaca.com");
@@ -24,6 +25,7 @@ TEST_CASE_METHOD(TestFixture, "Login with invalid credentials", "[example]") {
     automator.click("loginButton");
 
     QString expectedErrorMsg = "Your email or password is wrong";
-    REQUIRE(automator.waitUntil("passwordInput_errorLabel", "text", expectedErrorMsg, 10000));
+    REQUIRE_MSG(automator.waitUntil("passwordInput_errorLabel", "text", expectedErrorMsg, 10000),
+                "Error message should be: "+expectedErrorMsg.toUtf8());
 
 }
